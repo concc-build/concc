43dca5e4c7779f5451a49a48eff1bb08ed9b8c95
masnagam <masnagam@gmail.com>
2022-01-08 11:01:20 +0900

    feat!: merge netfs.override.yml into netfs.yml

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,7 +5,12 @@ apt-get install -y --no-install-recommends fuse3 gosu libglib2.0-0 ssh sshpass
 apt-get install -y --no-install-recommends strace  # for tracing syscalls
 apt-get install -y --no-install-recommends time  # for measurements
 
-sed -i 's|#PasswordAuthentication yes|PasswordAuthentication no|' /etc/ssh/sshd_config
+YQ_VERSION=v4.16.2
+YQ_BINARY=yq_linux_amd64
+YQ_DL_URL=https://github.com/mikefarah/yq/releases/download
+apt-get install -y --no-install-recommends ca-certificates curl
+curl -fsSL $YQ_DL_URL/$YQ_VERSION/$YQ_BINARY >/usr/local/bin/yq
+chmod +x /usr/local/bin/yq
 
 ssh-keygen -q -t ed25519 -N '' -f $HOME/.ssh/id_ed25519
 cp $HOME/.ssh/id_ed25519.pub $HOME/.ssh/authorized_keys
